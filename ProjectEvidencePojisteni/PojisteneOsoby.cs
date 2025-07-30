08ab65dc3aeed0bc59ccefab21f76ee27c71fa52
Martin Konupčík <konupcikmartin@gmail.com>
2025-07-30 12:14:36 +0200

    Add project files.

---
+++ ProjectEvidencePojisteni/PojisteneOsoby.cs
@@ -0,0 +1,20 @@
+﻿namespace EvidencePojisteni
+{
+    public class PojisteneOsoby
+    {
+
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+        public string Telefon { get; set; }
+        public int Vek { get; set; }
+
+
+        public PojisteneOsoby(string jmeno, string prijmeni, string telefon, int vek)
+        {
+            Jmeno = jmeno;
+            Prijmeni = prijmeni;
+            Telefon = telefon;
+            Vek = vek;
+        }
+    }
+}
