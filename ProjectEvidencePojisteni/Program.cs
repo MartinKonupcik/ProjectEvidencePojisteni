cfdfbbdebce6ad08639a9c3d796f8f4fef2bee14
Martin Konupčík <konupcikmartin@gmail.com>
2025-07-30 19:42:46 +0200

    Upravena verze Program.cs

--- ProjectEvidencePojisteni/Program.cs
+++ ProjectEvidencePojisteni/Program.cs
@@ -7,14 +7,15 @@ Console.WriteLine("Evidence Pojistenych");
 Console.WriteLine("---------------------------------");
 
 char choice = '0';
-while (choice != '4')
+while (choice != '5')
 {
     Console.WriteLine();
     Console.WriteLine("Vyberte si akci:");
     Console.WriteLine("1 - Přidat nového pojištěného");
     Console.WriteLine("2 - Vypsat všechny pojištěné");
-    Console.WriteLine("3 - Vyhledat pojištěného");
-    Console.WriteLine("4 - Konec");
+    Console.WriteLine("3 - Vyhledat pojištěného podle jmena i přijmeni");
+    Console.WriteLine("4 - Vyhledat pojištěného podle jmena nebo přijmeni");
+    Console.WriteLine("5 - Konec");
 
     choice = Console.ReadKey().KeyChar;
     Console.WriteLine();
@@ -30,15 +31,19 @@ while (choice != '4')
             break;
 
         case '3':
-            
+           
             break;
 
         case '4':
+            
+            break;
+        case '5':
             Console.WriteLine("Děkuji za použití aplikace.");
             break;
 
         default:
             Console.WriteLine("Neplatná volba. Zkuste to znovu.");
             break;
+
     }
 }
